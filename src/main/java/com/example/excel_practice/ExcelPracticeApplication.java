2f8fbeedf90f13084be4124d028c3e5023f6356b
ilmare-cbk <cbk104325@gmail.com>
2022-09-06 21:57:15 +0900

    init commit

---
+++ src/main/java/com/example/excel_practice/ExcelPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.excel_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/excel_practice/ExcelPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excel_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
